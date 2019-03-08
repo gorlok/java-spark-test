3a37583867a74f4470cbb15373f61f047409477d
Pablo Lillia <plillia@mpba.gov.ar>
2019-03-08 10:25:14 -0300

    .

--- src/main/java/com/example/spark/webserver/WebServer.java
+++ src/main/java/com/example/spark/webserver/WebServer.java
@@ -1,11 +1,48 @@
 package com.example.spark.webserver;
 
 
+import static spark.Spark.get;
+import static spark.Spark.halt;
+import static spark.Spark.post;
+import static spark.Spark.secure;
 import static spark.Spark.*;
 
 public class WebServer {
+	
 	public static void main(String[] args) {
 		secure("localhost.jks", "password", null, null);
+
+		// curl -k https://localhost:4567/hello
 		get("/hello", (req, res) -> "Hello World");
+		
+		// curl -i -k -X POST https://localhost:4567/login -d username=admin -d password=1234
+		post("/login", (req, res) -> {
+			if ("admin".equals(req.queryParams("username"))
+					&& "1234".equals(req.queryParams("password"))) {
+				req.session().attribute("authenticated", true);
+				return "logged in!";
+			} else {
+				halt(401);
+			}
+			return null;
+		});
+		
+		// try without auth:
+		// curl -v -k https://localhost:4567/protected/ping
+		//
+		// try with auth:
+		// curl -v -k -c cookies -X POST https://localhost:4567/login -d username=admin -d password=1234
+		// curl -v -k -b cookies https://localhost:4567/protected/ping
+		path("/protected", () -> {
+			get("/ping", (req, res) -> "pong!");
+		});
+		before("/protected/*", (req, rest) -> {
+			boolean authenticated = req.session().attribute("authenticated") != null;
+			
+			if (!authenticated) {
+				halt(401, "Go away!");
+			}
+		});
+		
 	}
 }
